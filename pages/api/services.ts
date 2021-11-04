5cf297642aa18b40f87b0efc9c39052e2aba0c15
oussama <bz.oussama@gmail.com>
2021-11-04 14:55:16 +0100

    initial deploy

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,8 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import { services } from '../../data';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(services);
+  res.status(200).json({ hey: 'hello' });
 }
