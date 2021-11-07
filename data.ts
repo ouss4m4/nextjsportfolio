3ce2f95a2958853d07c3392ce2e5f843fdd254b5
oussama <bz.oussama@gmail.com>
2021-11-07 17:36:26 +0100

    reordered projects

--- data.ts
+++ data.ts
@@ -2,72 +2,6 @@ import { IProject, ISkill, ITool } from './types';
 
 export const projects: IProject[] = [
   {
-    name: 'Clean Architecture Api',
-    category: ['Nodejs'],
-    deployed_url: 'https://ddd-sales.azurewebsites.net/api/v1',
-    github_url: 'https://github.com/ouss4m4/ddd-sales',
-    description:
-      'Domain-Driven Design with Clean Architecture Concepts API to create Clients Items and Orders. Architecture understanding is the GOAL of the api not the data',
-    image_path: '/images/clean-architecture.jpg',
-    tags: ['ExpressJs', 'Node', 'MongoDb', 'DDDesign'],
-    id: 1,
-  },
-  {
-    name: 'MicroServices API',
-    category: ['.Net'],
-    deployed_url: null,
-    github_url: 'https://github.com/ouss4m4/k8s-net-rabbitmq',
-    description:
-      'Microservice architecture built with .netcore 5 using rabbitmq as eventbus and docker-k8s for deployment',
-    image_path: '/images/k8s-ms-arch.png',
-    tags: ['Docker', 'Kubernetes', '.Net', 'RabbitMQ', 'gRPC', 'SQL'],
-    id: 2,
-  },
-  {
-    name: 'Sales Order WebApp',
-    category: ['.Net', 'React'],
-    deployed_url: 'https://salesorder-app.azurewebsites.net/',
-    github_url: 'https://github.com/ouss4m4/sales-order',
-    description:
-      'WebApp to create SalesOrders for clients. Built from scratch with Net Core and React. Deployed at Azure',
-    image_path: '/images/salesorder.png',
-    tags: ['C#', 'DotNet', 'React', 'TypeScript', 'Azure'],
-    id: 3,
-  },
-  {
-    name: 'Tournament Tracker',
-    category: ['Desktop', '.Net'],
-    deployed_url: null,
-    github_url: 'https://github.com/ouss4m4/TournamentTracker',
-    description:
-      'Desktop app built with WinForms on .NetCore to create players add them to teams, create tournaments using these teams and rewards, depending on number of starting teams,assign BYEs and rounds ',
-    image_path: '/images/TournamentTracker.JPG',
-    tags: ['.NetCore WinForms', 'Dapper', 'SqlServer'],
-    id: 4,
-  },
-  {
-    name: 'SnapShot',
-    category: ['Nodejs', 'React'],
-    deployed_url: 'https://mern-app.herokuapp.com/',
-    github_url: 'https://github.com/ouss4m4/snapShot',
-    description:
-      'Full Stack projects allows users to browse images. requires login to post and comment on other people photos. hosted on Heroku Free Tier slows the initial load',
-    image_path: '/images/snapshot.JPG',
-    tags: ['Heroku', 'React', 'MongoDb'],
-    id: 4,
-  },
-  {
-    name: 'Shopping List',
-    category: ['Mobile'],
-    deployed_url: 'https://shop-together-8d5e3.web.app',
-    github_url: 'https://github.com/ouss4m4/shopping_list',
-    description:
-      'Flutter cross platform app to create a shopping list that contains the item name , quantity and add a photo if needed',
-    image_path: '/images/shopping-list.png',
-    tags: ['Flutter', 'Dart', 'Android', 'iOS'],
-    id: 5,
-  },
-  {
     name: 'Task Manager',
     category: ['React'],
     deployed_url: 'https://master.d3e64dmv8w4bbk.amplifyapp.com/',
@@ -90,6 +24,39 @@ export const projects: IProject[] = [
     id: 7,
   },
   {
+    name: 'SnapShot',
+    category: ['Nodejs', 'React'],
+    deployed_url: 'https://mern-app.herokuapp.com/',
+    github_url: 'https://github.com/ouss4m4/snapShot',
+    description:
+      'Full Stack projects allows users to browse images. requires login to post and comment on other people photos. hosted on Heroku Free Tier slows the initial load',
+    image_path: '/images/snapshot.JPG',
+    tags: ['Heroku', 'React', 'MongoDb'],
+    id: 4,
+  },
+  {
+    name: 'Sales Order WebApp',
+    category: ['.Net', 'React'],
+    deployed_url: 'https://salesorder-app.azurewebsites.net/',
+    github_url: 'https://github.com/ouss4m4/sales-order',
+    description:
+      'WebApp to create SalesOrders for clients. Built from scratch with Net Core and React. Deployed at Azure',
+    image_path: '/images/salesorder.png',
+    tags: ['C#', 'DotNet', 'React', 'TypeScript', 'Azure'],
+    id: 3,
+  },
+  {
+    name: 'Shopping List',
+    category: ['Mobile'],
+    deployed_url: 'https://shop-together-8d5e3.web.app',
+    github_url: 'https://github.com/ouss4m4/shopping_list',
+    description:
+      'Flutter cross platform app to create a shopping list that contains the item name , quantity and add a photo if needed',
+    image_path: '/images/shopping-list.png',
+    tags: ['Flutter', 'Dart', 'Android', 'iOS'],
+    id: 5,
+  },
+  {
     name: 'GraphQL and .Net Core',
     category: ['.Net'],
     deployed_url: null,
@@ -98,6 +65,39 @@ export const projects: IProject[] = [
     image_path: '/images/gql-dotnet.png',
     tags: ['.Net', 'GraphQl', 'ASP.HotChocolate', 'SqlServer'],
     id: 8,
+  },
+  {
+    name: 'Clean Architecture Api',
+    category: ['Nodejs'],
+    deployed_url: 'https://ddd-sales.azurewebsites.net/api/v1',
+    github_url: 'https://github.com/ouss4m4/ddd-sales',
+    description:
+      'Domain-Driven Design with Clean Architecture Concepts API to create Clients Items and Orders. Architecture understanding is the GOAL of the api not the data',
+    image_path: '/images/clean-architecture.jpg',
+    tags: ['ExpressJs', 'Node', 'MongoDb', 'DDDesign'],
+    id: 1,
+  },
+  {
+    name: 'MicroServices API',
+    category: ['.Net'],
+    deployed_url: null,
+    github_url: 'https://github.com/ouss4m4/k8s-net-rabbitmq',
+    description:
+      'Microservice architecture built with .netcore 5 using rabbitmq as eventbus and docker-k8s for deployment',
+    image_path: '/images/k8s-ms-arch.png',
+    tags: ['Docker', 'Kubernetes', '.Net', 'RabbitMQ', 'gRPC', 'SQL'],
+    id: 2,
+  },
+  {
+    name: 'Tournament Tracker',
+    category: ['Desktop', '.Net'],
+    deployed_url: null,
+    github_url: 'https://github.com/ouss4m4/TournamentTracker',
+    description:
+      'Desktop app built with WinForms on .NetCore to create players add them to teams, create tournaments using these teams and rewards, depending on number of starting teams,assign BYEs and rounds ',
+    image_path: '/images/TournamentTracker.JPG',
+    tags: ['.NetCore WinForms', 'Dapper', 'SqlServer'],
+    id: 4,
   },
 ];
 
